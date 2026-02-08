f17f7faec62d28cdddf852bc7bbd89fd28366a81
farzona <aceracerov67@gmail.com>
2026-02-08 05:24:43 +0500

    change one

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,8 @@
 
 
 import React, { useState, useEffect } from 'react';
-import Jang from './images/jang.svg';
-import Jang2 from './images/Jang2.png';
+import Jang from '../About/images/jang.svg';
+import Jang2 from '../About/images/jang2.png';
 import './About.css';
 
 
